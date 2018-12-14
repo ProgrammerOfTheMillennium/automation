27d655f45d16f75f1495dbfaec0b31b7bb523adc
BuildTools <unconfigured@null.spigotmc.org>
2018-12-14 16:49:46 +0300

    Initial commit

---
+++ src/test/groovy/io/test/automation/LessonThree.groovy
@@ -0,0 +1,47 @@
+package io.test.automation
+
+import com.codeborne.selenide.CollectionCondition
+import com.codeborne.selenide.ElementsCollection
+import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.SelenideElement
+import org.junit.Test
+import org.openqa.selenium.By
+
+import static com.codeborne.selenide.Selenide.$
+import static com.codeborne.selenide.Selenide.$$
+
+/**
+ * Created on 14.12.2018
+ *
+ * @author Yuri Kudryavtsev
+ *         skype: yuri.kudryavtsev.indeed
+ *         email: ykudryavtsev@maxilect.com
+ */
+
+class LessonThree {
+    @Test
+    void scheduleTest() {
+        Selenide.open("https://rasp.yandex.ru/search")
+
+        def fromField = $("#from")
+        def toField = $("#to")
+        def whenField = $("#when")
+        def findButton = $("button.SearchForm__submit")
+
+        fromField.value = "Великий Новгород"
+        toField.value = "Москва"
+        whenField.value = "сегодня"
+        findButton.click()
+
+        def foundRecords = $$("article.SearchSegment_isVisible")
+
+        foundRecords.shouldBe(CollectionCondition.sizeGreaterThan(1))
+
+        foundRecords.each {
+            def recordName = it.$(By.className("SegmentTitle__header"))
+            def recordDuration = it.$(By.className("SearchSegment__duration"))
+
+            println("$recordName.text : $recordDuration.text")
+        }
+    }
+}
---
+++ src/test/groovy/io/test/automation/LessonTwo.groovy
@@ -0,0 +1,31 @@
+package io.test.automation
+
+
+import org.junit.Test
+import org.openqa.selenium.By
+
+import static com.codeborne.selenide.Selenide.*
+
+/**
+ * Created on 11.12.2018
+ *
+ * @author Yuri Kudryavtsev
+ *         skype: yuri.kudryavtsev.indeed
+ *         email: ykudryavtsev@maxilect.com
+ */
+class LessonTwo {
+
+    @Test
+    void searchTest() {
+        def searchRequest = "Cat"
+
+        open("https://google.com/")
+        def searchInput = $(By.name("q"))
+        searchInput.value = searchRequest
+        searchInput.pressEnter()
+
+        def found = $$("#ires .g")
+        assert found.size() > 0
+        assert found.first().text.contains(searchRequest)
+    }
+}
